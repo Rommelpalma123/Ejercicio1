aeb78883be8c3d98a15fe2da2f57ba2b66559ba3
Rommel-Palma <rommelpalma.3000@gmail.com>
2023-07-21 02:42:09 -0500

    fix: :bug: Generar Imagen
    Al presionar el boton de generar imagen debera aparece la imagen los con valores que se dio en los uinputs con el hamcho y alto

--- usuario/src/pages/User.jsx
+++ usuario/src/pages/User.jsx
@@ -1,5 +1,64 @@
+import { useStore } from "../store/apiStore"
+import { useState } from "react"
+
 export const User = () => {
+  const { width, height, imageData, fetchImageData } = useStore()
+  const [imageGenerated, setImageGenerated] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (width && height) {
+      fetchImageData(parseInt(width), parseInt(height))
+      setImageGenerated(true)
+    }
+  }
+
+  const inputs = {
+    display: "grid",
+    gap: "12px",
+    alignItems: "center"
+  }
+
+  const container = {
+    marginLeft: "800px"
+  }
   return (
-    <div>user</div>
+    <form
+      onSubmit={handleSubmit}
+      style={container}
+    >
+      <div style={inputs}>
+        <label htmlFor='width'>Ancho:</label>
+        <input
+          type='number'
+          id='width'
+          value={width}
+          onChange={(e) => useStore.setState({ width: e.target.value })}
+        />
+        <label htmlFor='height'>Alto:</label>
+        <input
+          type='number'
+          id='height'
+          value={height}
+          onChange={(e) => useStore.setState({ height: e.target.value })}
+        />
+        <button type='submit'>Generar Imagen</button>
+      </div>
+      <div>
+        {imageGenerated && imageData && (
+          <div>
+            <p>
+              Imagen generada con ancho: {width} y alto: {height}
+            </p>
+            <img
+              src={imageData}
+              alt='Imagen'
+              width={width}
+              height={height}
+            />
+          </div>
+        )}
+      </div>
+    </form>
   )
 }
